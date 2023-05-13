dd4f23ddb584af77715d2130ddc59460ecb4e44a
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2023-05-13 16:49:03 -0300

    Remove lazy-lock and convert telescope keybinds to lua functions

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,10 +5,10 @@ local M = {
 		{ "nvim-telescope/telescope-fzy-native.nvim" },
 	},
 	keys = {
-			{ '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>" },
-			{ '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
-			{ '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>" },
-			{ '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>" },
+			{ '<leader>ff', function() require('telescope.builtin').find_files() end },
+			{ '<leader>fg', function() require('telescope.builtin').live_grep() end },
+			{ '<leader>fb', function() require('telescope.builtin').buffers() end },
+			{ '<leader>fh', function() require('telescope.builtin').help_tags() end },
 	},
 	config = function(_, opts)
 		local telescope = require("telescope")
