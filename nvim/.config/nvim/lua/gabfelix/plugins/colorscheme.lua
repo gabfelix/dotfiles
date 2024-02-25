f5866229818a3b22e900401822be5828e8cab9c0
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-02-25 14:54:47 -0300

    Change colorscheme back to zenbones

--- nvim/.config/nvim/lua/gabfelix/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/gabfelix/plugins/colorscheme.lua
@@ -1,13 +1,15 @@
 return {
-	"luisiacc/gruvbox-baby",
+	"mcchrish/zenbones.nvim",
+	dependencies = { "lualine.nvim" },
 	lazy = false,
 	priority = 1000,
 	config = function()
+		vim.g.zenbones_compat = 1 -- Don't require lush.nvim
 		vim.opt.termguicolors = true
 		vim.opt.background = "dark"
 		require("lualine").setup({
-			theme = "gruvbox-baby"
+			theme = "zenbones"
 		})
-		vim.cmd.colorscheme("gruvbox-baby")
+		vim.cmd.colorscheme("zenbones")
 	end,
 }
