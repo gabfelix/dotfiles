0f5cb3e8b8560b83b3d433b8e59b59efa1064398
Gabriel Felix <git-commits.snowcap443@simplelogin.com>
2024-03-01 10:47:54 -0300

    nvim: Use fzf instead of fzy

--- nvim/.config/nvim/lua/gabfelix/plugins/telescope.lua
+++ nvim/.config/nvim/lua/gabfelix/plugins/telescope.lua
@@ -3,7 +3,17 @@ return {
 	cmd = "Telescope",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope-fzy-native.nvim" },
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+	},
+	opts = {
+			extensions = {
+				fzf = {
+					  fuzzy = true,                    -- false will only do exact matching
+					  override_generic_sorter = true,  -- override the generic sorter
+					  override_file_sorter = true,     -- override the file sorter
+					  case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+				}
+			}
 	},
 	keys = {
 			{ '<leader>ff', function() require('telescope.builtin').find_files() end },
@@ -14,6 +24,6 @@ return {
 	config = function(_, opts)
 		local telescope = require("telescope")
 		telescope.setup(opts)
-		telescope.load_extension("fzy_native")
+		telescope.load_extension("fzf")
 	end
 }
