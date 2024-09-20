15436a1ec73758cb336f1091f2ddf5d423ddb18c
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-09-20 19:50:05 -0300

    nvim: Lint restow.lua

--- nvim/.config/nvim/lua/config/restow.lua
+++ nvim/.config/nvim/lua/config/restow.lua
@@ -1,17 +1,17 @@
-local deployed_path = vim.fn.expand('$HOME/.config/nvim')
-local dotfiles_path = vim.fn.expand('$HOME/Code/dotfiles')
+local deployed_path = vim.fn.expand '$HOME/.config/nvim'
+local dotfiles_path = vim.fn.expand '$HOME/Code/dotfiles'
 
 -- Clean up broken symlinks in deployed config on startup
 if vim.fn.filereadable(deployed_path) then
-	local cmd = 'find ' .. deployed_path .. ' -type l ! -exec test -e {} \\; -delete'
-	vim.fn.system(cmd)
+  local cmd = 'find ' .. deployed_path .. ' -type l ! -exec test -e {} \\; -delete'
+  vim.fn.system(cmd)
 end
 
 -- Redeploy nvim config with stow wrapper script on change
 vim.api.nvim_create_autocmd('BufWritePost', {
-	pattern = dotfiles_path .. '/**',
-	callback = function()
-		local cmd = string.format('stow -v --no-folding -d %s nvim -t ~', dotfiles_path)
-		vim.cmd('!' .. cmd)
-	end
+  pattern = dotfiles_path .. '/**',
+  callback = function()
+    local cmd = string.format('stow -v --no-folding -d %s nvim -t ~', dotfiles_path)
+    vim.cmd('!' .. cmd)
+  end,
 })
