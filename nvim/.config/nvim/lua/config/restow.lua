22cad236cbb46bf62b787d060686e19ba9b53a36
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-09-20 14:35:29 -0300

    nvim: Refactor config deployment
    - Remove dependency on stowit.sh
    - Remove unnecessary directory changes
    - Run stow as vim shell command

--- nvim/.config/nvim/lua/config/restow.lua
+++ nvim/.config/nvim/lua/config/restow.lua
@@ -1,8 +1,5 @@
 local deployed_path = vim.fn.expand('$HOME/.config/nvim')
 local dotfiles_path = vim.fn.expand('$HOME/Code/dotfiles')
-local script_path = dotfiles_path .. '/stowit.sh'
-local augroup_name = 'DeployNvimDotfiles'
-
 
 -- Clean up broken symlinks in deployed config on startup
 if vim.fn.filereadable(deployed_path) then
@@ -11,16 +8,10 @@ if vim.fn.filereadable(deployed_path) then
 end
 
 -- Redeploy nvim config with stow wrapper script on change
-vim.api.nvim_create_augroup(augroup_name, { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
-	pattern = '*',
+	pattern = dotfiles_path .. '/**',
 	callback = function()
-		local file_path = vim.fn.expand('%:p')
-		if vim.fn.filereadable(dotfiles_path) and vim.fn.filereadable(script_path) and file_path:match('^' .. dotfiles_path .. '/') then
-			local original_cwd = vim.fn.getcwd()
-			vim.fn.chdir(dotfiles_path)
-			vim.fn.system('sh ' .. script_path .. ' nvim')
-			vim.fn.chdir(original_cwd)
-		end
+		local cmd = string.format('stow --no-folding -d %s nvim -t ~', dotfiles_path)
+		vim.cmd('!' .. cmd)
 	end
 })
