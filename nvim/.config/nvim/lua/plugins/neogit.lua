7f2ae1e98b2fdd078f5246ea7bc331ba5d0daa50
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-09-21 09:39:32 -0300

    nvim: Turn off spellcheck in Neogit  commit editor
    The ability to do this was literally added 4 days ago, I just got lucky
    looking at the GitHub file tree commit messages.

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -6,6 +6,11 @@ return {
     -- Only one of these is needed.
     'nvim-telescope/telescope.nvim',
   },
+  opts = {
+    commit_editor = {
+      spell_check = false,
+    },
+  },
   config = function(_, opts)
     require('neogit').setup(opts)
     vim.keymap.set('n', '<leader>G', require('neogit').open, { desc = 'Open Neo[G]it' })
