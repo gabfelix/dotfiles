e852da4c3f7a409d967274c0669b7cba971ebd19
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-10-01 11:07:31 -0300

    nvim: Comment out LSP config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,187 +1,188 @@
-return {
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-  { 'Bilal2453/luvit-meta', lazy = true },
-  {
-    -- Main LSP Configuration
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-      { 'j-hui/fidget.nvim', opts = {} },
-      'hrsh7th/cmp-nvim-lsp',
-    },
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-          -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-          map('<leader>e', vim.diagnostic.open_float, 'Show diagnostics')
-          map('<leader>q', vim.diagnostic.setloclist, 'Open diagnostic [Q]uickfix list')
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header.
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          -- Toggle inlay hints if supported
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
-      })
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local servers = {
-        clangd = {},
-        -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
-        ts_ls = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-            },
-          },
-        },
-      }
-
-      require('mason').setup()
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua',
-        'prettierd',
-        'eslint',
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- Overrides
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end,
-  },
-
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-    },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Move forward and backward in snippet expansion
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          {
-            name = 'lazydev',
-            -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-            group_index = 0,
-          },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     'folke/lazydev.nvim',
+--     ft = 'lua',
+--     opts = {
+--       library = {
+--         { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+--       },
+--     },
+--   },
+--   { 'Bilal2453/luvit-meta', lazy = true },
+--   {
+--     -- Main LSP Configuration
+--     'neovim/nvim-lspconfig',
+--     dependencies = {
+--       -- Automatically install LSPs and related tools to stdpath for Neovim
+--       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+--       'williamboman/mason-lspconfig.nvim',
+--       'WhoIsSethDaniel/mason-tool-installer.nvim',
+--       { 'j-hui/fidget.nvim', opts = {} },
+--       'hrsh7th/cmp-nvim-lsp',
+--     },
+--     config = function()
+--       vim.api.nvim_create_autocmd('LspAttach', {
+--         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+--         callback = function(event)
+--           local map = function(keys, func, desc, mode)
+--             mode = mode or 'n'
+--             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+--           end
+--           -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+--           -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+--           -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+--           -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+--           -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+--           -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+--           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+--           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+--           map('<leader>e', vim.diagnostic.open_float, 'Show diagnostics')
+--           map('<leader>q', vim.diagnostic.setloclist, 'Open diagnostic [Q]uickfix list')
+--           -- WARN: This is not Goto Definition, this is Goto Declaration.
+--           --  For example, in C this would take you to the header.
+--           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+--
+--           local client = vim.lsp.get_client_by_id(event.data.client_id)
+--           -- Toggle inlay hints if supported
+--           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+--             map('<leader>th', function()
+--               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+--             end, '[T]oggle Inlay [H]ints')
+--           end
+--         end,
+--       })
+--
+--       local capabilities = vim.lsp.protocol.make_client_capabilities()
+--       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+--       local servers = {
+--         clangd = {},
+--         -- gopls = {},
+--         pyright = {},
+--         rust_analyzer = {},
+--         ts_ls = {},
+--         lua_ls = {
+--           settings = {
+--             Lua = {
+--               completion = {
+--                 callSnippet = 'Replace',
+--               },
+--             },
+--           },
+--         },
+--       }
+--
+--       require('mason').setup()
+--       local ensure_installed = vim.tbl_keys(servers or {})
+--       vim.list_extend(ensure_installed, {
+--         'stylua',
+--         'prettierd',
+--         'eslint',
+--       })
+--       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+--
+--       require('mason-lspconfig').setup {
+--         handlers = {
+--           function(server_name)
+--             local server = servers[server_name] or {}
+--             -- Overrides
+--             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+--             require('lspconfig')[server_name].setup(server)
+--           end,
+--         },
+--       }
+--     end,
+--   },
+--
+--   { -- Autocompletion
+--     'hrsh7th/nvim-cmp',
+--     event = 'InsertEnter',
+--     dependencies = {
+--       -- Snippet Engine & its associated nvim-cmp source
+--       {
+--         'L3MON4D3/LuaSnip',
+--         build = (function()
+--           -- Build Step is needed for regex support in snippets.
+--           -- This step is not supported in many windows environments.
+--           -- Remove the below condition to re-enable on windows.
+--           if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+--             return
+--           end
+--           return 'make install_jsregexp'
+--         end)(),
+--         dependencies = {
+--           -- `friendly-snippets` contains a variety of premade snippets.
+--           --    See the README about individual language/framework/plugin snippets:
+--           --    https://github.com/rafamadriz/friendly-snippets
+--           -- {
+--           --   'rafamadriz/friendly-snippets',
+--           --   config = function()
+--           --     require('luasnip.loaders.from_vscode').lazy_load()
+--           --   end,
+--           -- },
+--         },
+--       },
+--       'saadparwaiz1/cmp_luasnip',
+--
+--       -- Adds other completion capabilities.
+--       --  nvim-cmp does not ship with all sources by default. They are split
+--       --  into multiple repos for maintenance purposes.
+--       'hrsh7th/cmp-nvim-lsp',
+--       'hrsh7th/cmp-path',
+--     },
+--     config = function()
+--       -- See `:help cmp`
+--       local cmp = require 'cmp'
+--       local luasnip = require 'luasnip'
+--       luasnip.config.setup {}
+--       cmp.setup {
+--         snippet = {
+--           expand = function(args)
+--             luasnip.lsp_expand(args.body)
+--           end,
+--         },
+--         completion = { completeopt = 'menu,menuone,noinsert' },
+--         mapping = cmp.mapping.preset.insert {
+--           -- Select the [n]ext item
+--           ['<C-n>'] = cmp.mapping.select_next_item(),
+--           -- Select the [p]revious item
+--           ['<C-p>'] = cmp.mapping.select_prev_item(),
+--
+--           -- Scroll the documentation window [b]ack / [f]orward
+--           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+--           ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--
+--           -- Accept ([y]es) the completion.
+--           ['<C-y>'] = cmp.mapping.confirm { select = true },
+--
+--           -- Manually trigger a completion from nvim-cmp.
+--           --  Generally you don't need this, because nvim-cmp will display
+--           --  completions whenever it has completion options available.
+--           ['<C-Space>'] = cmp.mapping.complete {},
+--
+--           -- Move forward and backward in snippet expansion
+--           ['<C-l>'] = cmp.mapping(function()
+--             if luasnip.expand_or_locally_jumpable() then
+--               luasnip.expand_or_jump()
+--             end
+--           end, { 'i', 's' }),
+--           ['<C-h>'] = cmp.mapping(function()
+--             if luasnip.locally_jumpable(-1) then
+--               luasnip.jump(-1)
+--             end
+--           end, { 'i', 's' }),
+--         },
+--         sources = {
+--           {
+--             name = 'lazydev',
+--             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+--             group_index = 0,
+--           },
+--           { name = 'nvim_lsp' },
+--           { name = 'luasnip' },
+--           { name = 'path' },
+--         },
+--       }
+--     end,
+--   },
+-- }
