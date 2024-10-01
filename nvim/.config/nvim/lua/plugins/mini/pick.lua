e2341586fa87b2f94e21cee57d78cd27c657937b
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-10-01 10:22:29 -0300

    nvim: Add mini.extra lsp pickers maps to mini.pick config

--- nvim/.config/nvim/lua/plugins/mini/pick.lua
+++ nvim/.config/nvim/lua/plugins/mini/pick.lua
@@ -1,8 +1,9 @@
 return {
   'echasnovski/mini.pick',
-  version = '*',
+  version = false,
   dependencies = {
     'echasnovski/mini.icons',
+    { 'echasnovski/mini.extra', version = false }, -- More pickers
   },
   opts = {
     mappings = {
@@ -14,5 +15,17 @@ return {
     vim.keymap.set('n', '<leader>f', MiniPick.builtin.files, { desc = 'pick [f]iles' })
     vim.keymap.set('n', '<leader><leader>', MiniPick.builtin.buffers, { desc = 'pick buffers' })
     vim.keymap.set('n', '<leader>g', MiniPick.builtin.grep_live, { desc = 'pick grep' })
+
+    require('mini.extra').setup {}
+    local lsp_document_symbols = function()
+      MiniExtra.pickers.lsp { scope = 'document_symbol' }
+    end
+
+    local lsp_references = function()
+      MiniExtra.pickers.lsp { scope = 'references' }
+    end
+
+    vim.keymap.set('n', '<leader>ld', lsp_document_symbols, { desc = 'pick [l]SP [d]ocument symbols' })
+    vim.keymap.set('n', '<leader>lr', lsp_references, { desc = 'pick [l]SP [r]eferences' })
   end,
 }
