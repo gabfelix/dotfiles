8a8d291113fa24965e7e9428a351269b5e8f3069
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-01-11 14:40:59 -0300

    emacs: Remove package manager and switch to cobalt2
    I'm bundling a cobalt2 theme with the config and then modifying it
    to have green comments in my init.el, which is similar to a setup I
    have in VS Code which I've been pretty happy with as of late.
    
    Also removing the package management stuff because it just bloats up
    my editor and I don't use it right now. Will MELPA back if it changes.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,61 +1,24 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
+;; Remove junk
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
+;(scroll-bar-mode -1)
 
-(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-directory-alist `(("." . "~/.emacs_saves")))
 
-;; Appearance
-(load-theme 'modus-vivendi t)
-(set-face-attribute 'default nil :height 120)
+(load-theme 'cobalt2 t)
+(custom-set-faces
+ '(font-lock-comment-face ((t (:foreground "#3AD900"))))
+ '(font-lock-doc-face ((t (:foreground "#3AD900"))))
+)
+(when (eq system-type 'windows-nt)
+  (set-frame-font "Consolas 10" nil t))
 
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
+;; Have emacs change custom.el instead of here
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
-
-;;(use-package typescript-mode)
-(use-package markdown-mode
-  :ensure t)
-(use-package magit
-  :ensure t)
-
-;; elfeed - RSS reader
-(use-package elfeed
-  :ensure t
-  :config
-  (defun elfeed-play-with-mpv ()
-    "Play entry link with mpv."
-    (interactive)
-    (let ((entry (if (eq major-mode 'elfeed-show-mode) elfeed-show-entry (elfeed-search-selected :single))))
-      (message "Opening %s with haruna..." (elfeed-entry-link entry))
-      (start-process "elfeed-mpv" nil "haruna" (elfeed-entry-link entry))))
-
-  (defvar elfeed-mpv-patterns
-    '("youtu\\.?be")
-    "List of regexp to match against elfeed entry link to know
-whether to use mpv to visit the link.")
-
-  (defun elfeed-visit-or-play-with-mpv ()
-    "Play in mpv if entry link matches elfeed-mpv-patterns', visit otherwise.
-See elfeed-play-with-mpv'."
-    (interactive)
-    (let ((entry (if (eq major-mode 'elfeed-show-mode) elfeed-show-entry (elfeed-search-selected :single)))
-          (patterns elfeed-mpv-patterns))
-      (while (and patterns (not (string-match (car elfeed-mpv-patterns) (elfeed-entry-link entry))))
-	(setq patterns (cdr patterns)))
-      (if patterns
-          (elfeed-play-with-mpv)
-	(if (eq major-mode 'elfeed-search-mode)
-            (elfeed-search-browse-url)
-          (elfeed-show-visit)))))
-  )
