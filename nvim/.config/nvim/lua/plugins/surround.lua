8525ac028c4f8e0540a98e42a39b631ca89a61e2
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-02-11 07:59:25 -0300

    nvim: Add surround reference comments

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,4 +1,9 @@
 return {
+  -- Quick reference:
+  -- Delete surrounding with `sd`.
+  -- Replace surrounding with `sr`.
+  -- Find surrounding with `sf` or `sF` (move cursor right or left).
+  -- See <https://github.com/echasnovski/mini.surround> for more information
   'echasnovski/mini.surround',
   opts = {},
 }
