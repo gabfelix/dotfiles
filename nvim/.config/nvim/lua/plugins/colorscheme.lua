dea86713419ab4eecce273cb89029b81d28d6e55
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-02-27 21:32:52 -0300

    nvim: Change colorscheme to modus

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  'folke/tokyonight.nvim',
+  'miikanissi/modus-themes.nvim',
   priority = 1000,
   init = function()
-    require('tokyonight').setup()
     vim.o.background = 'dark'
-    -- You can also load 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'modus'
   end,
 }
