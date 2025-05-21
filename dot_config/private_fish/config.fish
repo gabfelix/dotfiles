bb22845c224660a8a22cba763263e48058110b73
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-05-20 22:04:39 -0300

    tweaks

--- dot_config/private_fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-	fish_vi_key_bindings
-end
-
-export EDITOR="nvim"
-
-source /usr/share/ublue-os/bling/bling.fish
