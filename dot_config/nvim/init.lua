686dbb047296d992f325be138d6e8a768c3ffab6
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-05-21 21:25:02 -0300

    nvim: Add treesitter

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -185,6 +185,42 @@ require("lazy").setup({
 			end,
 		}
 	},
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		main = 'nvim-treesitter.configs',
+		opts = {
+			ensure_installed = {
+				'bash',
+				'c',
+				'cpp',
+				'diff',
+				'html',
+				'lua',
+				'luadoc',
+				'markdown',
+				'markdown_inline',
+				'query',
+				'vim',
+				'vimdoc',
+				'toml',
+				'yaml',
+				'ini',
+				'rust',
+				'javascript',
+				'typescript',
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = function(_, bufnr)
+					return vim.api.nvim_buf_line_count(bufnr) > 50000
+				end,
+				additional_vim_regex_highlighting = { 'ruby' },
+			},
+			indent = { enable = false },
+		}
+	},
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
