e19076a5c2f45ef6e3e8829b440191b6c9dd24f9
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-05-29 18:06:37 -0300

    Fire up emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,30 +7,27 @@
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-;(scroll-bar-mode -1)
+(scroll-bar-mode -1)
 
-(setq backup-directory-alist `(("." . "~/.emacs_saves")))
-
-;; Appearance
-(load-theme 'cobalt2 t)
-(custom-set-faces
- '(font-lock-comment-face ((t (:foreground "#3AD900"))))
- '(font-lock-doc-face ((t (:foreground "#3AD900"))))
-)
-(when (eq system-type 'windows-nt)
-  (set-frame-font "Roboto Mono 11" nil t))
-
+;; Ido completion
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+;; Load theme with f7
+(global-set-key [f7] (lambda () (interactive) (find-file user-init-file)))
+
+(setq backup-directory-alist `(("." . "~/.emacs_saves")))
+
+(load-theme 'modus-vivendi t)
+(set-face-attribute 'default nil
+                    :family "DejaVu Sans Mono"
+		    :height 120)
+
+(use-package magit
+  :ensure t)
 
 ;; Have emacs change custom.el instead of here
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
-
-(use-package typescript-mode
-  :ensure t)
-
-(use-package magit
-  :ensure t)
