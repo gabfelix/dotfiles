b1fad34887d337f4499371fb7e8d9c5c147945c5
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-05-30 17:00:01 -0300

    bro thinks hes jblow

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,18 +14,28 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;; Load theme with f7
+;; Edit init.el with f7
 (global-set-key [f7] (lambda () (interactive) (find-file user-init-file)))
 
 (setq backup-directory-alist `(("." . "~/.emacs_saves")))
 
-(load-theme 'modus-vivendi t)
+(use-package naysayer-theme
+  :ensure t
+  :config
+  (load-theme 'naysayer t))
+
 (set-face-attribute 'default nil
                     :family "DejaVu Sans Mono"
-		    :height 120)
+		    :height 110)
 
 (use-package magit
   :ensure t)
+
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq typescript-indent-level 2)
+  (setq typescript-auto-indent-flag t))
 
 ;; Have emacs change custom.el instead of here
 (setq custom-file (concat user-emacs-directory "custom.el"))
