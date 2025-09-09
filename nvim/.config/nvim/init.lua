22491136bb5e2d8b0d9e2a23b70d3cd271e796a7
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-09-09 08:54:50 -0300

    nvim: Add neovide settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,3 +2,6 @@ require 'config.base' -- Load this before anything else
 require 'config.restow' -- Automatically redeploy nvim config
 require 'config.lazy'
 require 'config.markdown-preview'
+if vim.g.neovide then
+  require 'config.neovide'
+end
