2eb4ddcd9147da5f9fef4287a5aed574b326c847
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-10-05 11:19:47 -0300

    nvim: Remove Obsidian replacement stuff
    Just use Obsidian.

--- nvim/.config/nvim/lua/plugins/equations.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Thiago4532/mdmath.nvim',
-  ft = 'markdown',
-  cond = vim.fn.executable 'magick' and vim.fn.executable 'rsvg-convert',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-  },
-  opts = { filetypes = { 'markdown' } },
-}
