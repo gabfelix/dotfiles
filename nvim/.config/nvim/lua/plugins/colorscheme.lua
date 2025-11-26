dd26a2a48375fb7bb6a3f74f871ed63cde1a36c5
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-11-26 20:47:20 -0300

    nvim: Add gruvbox transparency

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
   'ellisonleao/gruvbox.nvim',
   priority = 1000,
   config = function()
+    require('gruvbox').setup {
+      transparent_mode = true,
+    }
     vim.o.background = 'dark'
     vim.cmd.colorscheme 'gruvbox'
   end,
