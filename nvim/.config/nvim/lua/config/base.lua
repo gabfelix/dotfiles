d0e90d15b60cd7a6d0592c090ae6217f6ee96b51
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-12-08 11:01:30 -0300

    nvim(base): QoL keybindings

--- nvim/.config/nvim/lua/config/base.lua
+++ nvim/.config/nvim/lua/config/base.lua
@@ -19,6 +19,25 @@ vim.opt.wildmode = { 'longest:full', 'full' }
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlighting' })
+
+-- Quickfix
+vim.keymap.set('n', '<leader>q', '<cmd>botright copen<CR>', { noremap = true, desc = 'Open Quickfix list (bottom right)' })
+vim.keymap.set('n', '<leader>Q', '<cmd>cclose<CR>', { noremap = true, desc = 'Close Quickfix list' })
+-- Open/close location list
+vim.keymap.set('n', '<leader>l', '<cmd>lopen<CR>', { noremap = true, desc = 'Open Location list' })
+vim.keymap.set('n', '<leader>L', '<cmd>lclose<CR>', { noremap = true, desc = 'Close Location list' })
+
+-- Tabs
+vim.keymap.set('n', '<leader>t', '<cmd>tabnew<CR>', { noremap = true, desc = 'Open new empty tab' })
+vim.keymap.set('n', '<leader>T', '<cmd>tabclose<CR>', { noremap = true, desc = 'Close current tab' })
+
+-- F7 to edit main config
+vim.keymap.set('n', '<F7>', function()
+    local config_dir = vim.fn.stdpath("config")
+    vim.cmd("cd " .. config_dir)
+    local init_file_path = config_dir .. "/init.lua"
+    vim.cmd("edit " .. init_file_path)
+end, { desc = 'Edit init.lua and change CWD' })
