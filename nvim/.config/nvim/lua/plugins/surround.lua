996ca4d1219247bad2b2891595d4fbf267c684be
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2025-12-08 11:13:47 -0300

    nvim: Add desc fields to all keybindings
    Or at least the ones I found with grep

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -24,9 +24,8 @@ return {
     }
 
     -- Remap adding surrounding to Visual mode selection
-    vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
-
+    vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true, desc = 'Add surround to selection' })
     -- Make special mapping for "add surrounding for line"
-    vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false })
+    vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false, desc = 'Add surround to line' })
   end,
 }
