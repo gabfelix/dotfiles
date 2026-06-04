3863c3ca24ea03f70ff96dc19c765d61c99e76d3
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2026-06-04 04:53:04 -0300

    nvim: Use ellisonleao/gruvbox with transparency

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,50 +18,60 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.hlsearch = true
 
-vim.cmd.colorscheme 'retrobox'
+vim.o.timeout = true
+vim.o.timeoutlen = 200
+vim.o.ttimeoutlen = 10
 
 -- F7 to edit main config
 vim.keymap.set('n', '<F7>', function()
-	local config_dir = vim.fn.stdpath("config")
-	vim.cmd("cd " .. config_dir)
-	local init_file_path = config_dir .. "/init.lua"
-	vim.cmd("edit " .. init_file_path)
+  local config_dir = vim.fn.stdpath("config")
+  vim.cmd("cd " .. config_dir)
+  local init_file_path = config_dir .. "/init.lua"
+  vim.cmd("edit " .. init_file_path)
 end, { desc = 'Edit init.lua and change CWD' })
 
 vim.api.nvim_create_autocmd('PackChanged', {
-	callback = function(ev)
-		local name, kind = ev.data.spec.name, ev.data.kind
-		if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
-			vim.system({ 'make' }, { cwd = ev.data.path })
-		end
-	end,
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
+      vim.system({ 'make' }, { cwd = ev.data.path })
+    end
+  end,
 })
 
 vim.pack.add({
-	'https://github.com/nvim-mini/mini.surround',
-	'https://github.com/stevearc/oil.nvim',
-	'https://github.com/nvim-lua/plenary.nvim', -- telescope dependency
-	'https://github.com/nvim-telescope/telescope.nvim',
-	'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/nvim-mini/mini.surround',
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/nvim-lua/plenary.nvim', -- telescope dependency
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/ellisonleao/gruvbox.nvim',
 })
 
+vim.opt.background = 'dark'
+require("gruvbox").setup({
+  transparent_mode = true,
+})
+vim.cmd.colorscheme 'gruvbox'
+
+
 require('mini.surround').setup {
-	custom_surroundings = {
-		['('] = { output = { left = '( ', right = ' )' } },
-		['['] = { output = { left = '[ ', right = ' ]' } },
-		['{'] = { output = { left = '{ ', right = ' }' } },
-		['<'] = { output = { left = '< ', right = ' >' } },
-	},
-	mappings = {
-		add = 'ys',
-		delete = 'ds',
-		find = '',
-		find_left = '',
-		highlight = '',
-		replace = 'cs',
-		update_n_lines = '',
-	},
-	search_method = 'cover_or_next',
+  custom_surroundings = {
+    ['('] = { output = { left = '( ', right = ' )' } },
+    ['['] = { output = { left = '[ ', right = ' ]' } },
+    ['{'] = { output = { left = '{ ', right = ' }' } },
+    ['<'] = { output = { left = '< ', right = ' >' } },
+  },
+  mappings = {
+    add = 'ys',
+    delete = 'ds',
+    find = '',
+    find_left = '',
+    highlight = '',
+    replace = 'cs',
+    update_n_lines = '',
+  },
+  search_method = 'cover_or_next',
 }
 -- Remap adding surrounding to Visual mode selection
 vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true, desc = 'Add surround to selection' })
@@ -69,14 +79,14 @@ vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], 
 vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false, desc = 'Add surround to line' })
 
 require('oil').setup {
-	default_file_explorer = true, -- Take over from netrw
-	columns = { 'icon' },
-	view_options = {
-		show_hidden = true,
-	},
-	keymaps = {
-		['zh'] = 'actions.toggle_hidden',
-	},
+  default_file_explorer = true, -- Take over from netrw
+  columns = { 'icon' },
+  view_options = {
+    show_hidden = true,
+  },
+  keymaps = {
+    ['zh'] = 'actions.toggle_hidden',
+  },
 }
 vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Open parent directory in Oil' })
 
