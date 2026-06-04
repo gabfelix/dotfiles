18402a4fcf7af707a73aa7ee46db417fde9b5ee5
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2026-06-04 04:52:21 -0300

    hypr: Tweak godot window rules

--- hyprland/.config/hypr/rules.lua
+++ hyprland/.config/hypr/rules.lua
@@ -19,6 +19,22 @@ hl.window_rule({
 
 hl.window_rule({
     name  = "force-tile",
-    match = { class = "^(Aseprite|Godot)$" },
+    match = { class = "^(Aseprite)$" },
     tile  = true,
 })
+
+hl.window_rule({
+  name = "Godot Float Internal Windows",
+  float = true,
+  tile = false,
+  match = { class = "^Godot$" , initial_title = nil }, -- keep generic; filtered by next rule
+})
+
+hl.window_rule({
+  name = "Godot Tile Main Window",
+  float = false,
+  tile = true,
+  match = { class = "^Godot$", initial_title = "^Godot$" },
+})
+
+
